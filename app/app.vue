2eb817b14f068dfa9ff587d0d5669e2a9101862f
RaulSAraujo <raul.araujo.2000@hotmail.com>
2026-01-28 14:49:58 -0300

    ✨ feat: atualização da configuração do app e melhorias na SEO

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const color = computed(() => colorMode.value === 'dark' ? '#020618' : 'white')
+
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: color }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'pt-BR'
+  }
+})
+
+useSeoMeta({
+  titleTemplate: '%s - Raul Araujo',
+  ogImage: 'https://github.com/RaulSAraujo.png'
+})
+</script>
 
 <template>
   <UApp>
     <NuxtLayout>
-      <UMain>
+      <UMain class="relative">
         <NuxtPage />
       </UMain>
     </NuxtLayout>
